0fd675db6601317b25fb0dc9175402cbb314b243
frank-liu-1 <liushaomingdev@163.com>
2017-03-06 16:04:45 +0800

    Init

---
+++ src/main/java/com/frank/sboot/Config.java
@@ -0,0 +1,15 @@
+package com.frank.sboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * 
+ * @author Frank Liu
+ *
+ */
+@Configuration
+@ImportResource(locations={"classpath:spring/appContext.xml"})
+public class Config {
+
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/view/hello.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<body>
+	<a href="www.baidu.com">百度</a>
+</body>
+</html>
