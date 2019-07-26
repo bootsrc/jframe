1ed3c3388f77d1f74ba7e65eb7667447069f1c9c
liushaoming <liushaomingdev@163.com>
2019-07-26 17:51:14 +0800

    add commits

--- nio-demo/src/main/java/com/lsm/nio/ChannelCopy.java
+++ nio-demo/src/main/java/com/lsm/nio/ChannelCopy.java
@@ -18,6 +18,13 @@ public class ChannelCopy {
     }
 
     private static void copy1(ReadableByteChannel src, WritableByteChannel des) throws Exception {
+        /*
+            使用Buffer读写数据一般需要循环执行以下四个步骤：
+            1.写入数据到Buffer
+            2.调用flip()方法
+            3.从Buffer中读取数据
+            4.调用compact()方法
+        */
 
         ByteBuffer tmp = ByteBuffer.allocate(16 * 1024);
         while (src.read(tmp) != -1) {
