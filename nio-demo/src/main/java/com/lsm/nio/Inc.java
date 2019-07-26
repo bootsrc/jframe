78d345fde4bcdfbdbd0116edf4fd019a9a1f6fc5
liushaoming <liushaomingdev@163.com>
2019-07-26 17:27:27 +0800

    nio ByteBuffer demo

---
+++ nio-demo/src/main/java/com/lsm/nio/Inc.java
@@ -0,0 +1,25 @@
+package com.lsm.nio;
+
+/**
+ * @author ：Frank
+ * @date ：Created in 2019/7/26 16:47
+ * @description：
+ * @modified By：
+ */
+public class Inc {
+/*
+    简写：
+    i = i++;
+    /////// 分解如下
+    int temp = i;
+    i =i+ 1;
+    int i = temp;
+*/
+
+    public static void main(String[] args) {
+        int i=0;
+
+        i = i++;
+        System.out.println(i);
+    }
+}
